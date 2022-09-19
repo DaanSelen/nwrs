98a9066285a03931c25acad16c5494a3648e353e
Daan Selen <selendaan@hotmail.com>
2022-09-19 15:45:20 -0500

    Update 0.172

--- scripts/removeUser.sh
+++ scripts/removeUser.sh
@@ -9,7 +9,7 @@ if [ -z "$username" ]
 then
     echo "Missing argument"
 else
-    deluser $username
+    /usr/sbin/deluser $username
     rm -rfd /usr/local/nwrs/$username; rm -rfd /usr/local/nwrs/web/$username
     sed -i "/DenyUsers $username/d" /etc/ssh/sshd_config
     sed -i "/$username/d" var/users
