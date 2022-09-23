75b21552ae594b9c086fc8b55acb90715d7b2a03
= <=>
2022-09-23 10:46:51 +0200

    Update 0.22

--- api-server/main.go
+++ api-server/main.go
@@ -1,15 +1,29 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"net/http"
 	"os/exec"
 
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 )
 
+const (
+	ipaddr      = "localhost:3306"
+	auth        = "nwrsuser:Welkom01"
+	workingbase = "nwrs"
+)
+
+var (
+	nwrsdb *sql.DB
+	err    error
+)
+
 func main() {
+	initDBConnection()
 	initHttp()
 }
 
@@ -22,9 +36,21 @@ func initHttp() {
 	WRS.HandleFunc("/wrs/user", manipulateUser("DELETE")).Methods("DELETE")
 	WRS.HandleFunc("/wrs/container", manipulateContainer("CREATE")).Methods("POST")
 	WRS.HandleFunc("/wrs/container", manipulateContainer("DELETE")).Methods("DELETE")
-	WRS.HandleFunc("/wrs/portcount", resetPort).Methods("GET")
+	WRS.HandleFunc("/wrs/portcount", resetPort).Methods("PATCH")
 
 	http.ListenAndServe((":1234"), WRS)
+}
+
+func initDBConnection() {
+	fmt.Println("Attempting MariaDB Connection")
+	nwrsdb, err = sql.Open("mysql", auth+"@tcp("+ipaddr+")/"+workingbase)
+	if err != nil {
+		panic("[ERROR] CONNECTING TO DATABASE FAILED")
+	}
+	_, err := nwrsdb.Exec("CREATE TABLE IF NOT EXISTS user (id INTEGER PRIMARY KEY AUTO_INCREMENT, username TEXT, passwd TEXT)")
+	if err != nil {
+		panic(err)
+	}
 }
 
 func rootEndpoint(w http.ResponseWriter, _ *http.Request) {
@@ -36,12 +62,19 @@ func manipulateUser(command string) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		uQuery, ok1 := r.URL.Query()["user"]
 		pQuery, ok2 := r.URL.Query()["pass"]
-		if (ok1 || len(uQuery) > 0 && ok2 || len(pQuery) > 0) && command == "CREATE" {
-			executeBash("/usr/local/nwrs/scripts/createUser.sh -u "+uQuery[0]+" -p "+pQuery[0], true)
-			json.NewEncoder(w).Encode("CREATE USER")
-		} else if ok1 || len(uQuery) > 0 && command == "REMOVE" {
-			executeBash("/usr/local/nwrs/scripts/removeUser.sh -u "+uQuery[0], true)
-			json.NewEncoder(w).Encode("REMOVE USER")
+		if ok1 || len(uQuery) > 0 && ok2 || len(pQuery) > 0 {
+			switch command {
+			case "CREATE":
+				executeBash("/usr/local/nwrs/scripts/createUser.sh -u "+uQuery[0]+" -p "+pQuery[0], true)
+				manipulateData("CREATE", uQuery[0], pQuery[0])
+				json.NewEncoder(w).Encode("CREATE USER")
+			case "DELETE":
+				if checkAuth(uQuery[0], pQuery[0]) {
+					executeBash("/usr/local/nwrs/scripts/removeUser.sh -u "+uQuery[0], true)
+					manipulateData("REMOVE", uQuery[0], pQuery[0])
+					json.NewEncoder(w).Encode("REMOVE USER")
+				}
+			}
 		} else {
 			w.WriteHeader(400)
 		}
@@ -50,15 +83,18 @@ func manipulateUser(command string) http.HandlerFunc {
 
 func manipulateContainer(command string) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		uQuery, ok := r.URL.Query()["user"]
-		if ok || len(uQuery) > 0 {
-			switch command {
-			case "CREATE":
-				executeBash("/usr/local/nwrs/scripts/createContainer.sh -u "+uQuery[0], true)
-				json.NewEncoder(w).Encode("CREATE CONTAINER")
-			case "DELETE":
-				executeBash("/usr/local/nwrs/scripts/removeContainer.sh -u "+uQuery[0], true)
-				json.NewEncoder(w).Encode("DELETE CONTAINER")
+		uQuery, ok1 := r.URL.Query()["user"]
+		pQuery, ok2 := r.URL.Query()["pass"]
+		if ok1 || len(uQuery) > 0 && ok2 || len(pQuery) > 0 {
+			if checkAuth(uQuery[0], pQuery[0]) {
+				switch command {
+				case "CREATE":
+					executeBash("/usr/local/nwrs/scripts/createContainer.sh -u "+uQuery[0], true)
+					json.NewEncoder(w).Encode("CREATE CONTAINER")
+				case "DELETE":
+					executeBash("/usr/local/nwrs/scripts/removeContainer.sh -u "+uQuery[0], true)
+					json.NewEncoder(w).Encode("DELETE CONTAINER")
+				}
 			}
 		} else {
 			w.WriteHeader(400)
@@ -70,6 +106,34 @@ func resetPort(w http.ResponseWriter, _ *http.Request) {
 	output := executeBash("/usr/local/nwrs/scripts/resetPort.sh", false)
 	json.NewEncoder(w).Encode(("PortReset finished, status: " + output))
 	fmt.Println("Done")
+}
+
+func manipulateData(command, username, passwd string) {
+	if command == "CREATE" {
+		_, err := nwrsdb.Exec("INSERT INTO user(username, passwd) VALUES('" + username + "', '" + passwd + "')")
+		if err != nil {
+			panic(err)
+		}
+	} else if command == "REMOVE" {
+		_, err := nwrsdb.Exec("DELETE FROM user WHERE username = '" + username + "'")
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+func checkAuth(username, tryPasswd string) bool {
+	var passwd string
+	err = nwrsdb.QueryRow("SELECT passwd FROM user WHERE username = '" + username + "'").Scan(&passwd)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(tryPasswd == passwd)
+	if tryPasswd == passwd {
+		return true
+	} else {
+		return false
+	}
 }
 
 func executeBash(path string, special bool) string {
--- setup/setup.sh
+++ setup/setup.sh
@@ -1,8 +1,7 @@
 #!bin/bash
 sudo echo "This needs to be run as admin."
-apt-get install docker.io -y
-apt-get install vsftpd -y; systemctl enable vsftpd
-cat ./vsftpd.conf > /etc/vsftpd.conf; touch /etc/vsftpd.chroot_list
+apt-get install docker.io vsftpd -y; systemctl enable vsftpd
+cp ./vsftpd.conf /etc/vsftpd.conf; touch /etc/vsftpd.chroot_list
 chmod 444 /etc/vsftpd.chroot_list
 chmod 666 /usr/local/nwrs/scripts/var/*
 
