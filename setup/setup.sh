9bec6dd0178673a75d51cdb9f7faf5ce32812f0f
celdrith <dselen@nerthus.nl>
2022-11-02 16:08:27 +0100

    Update 0.25

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,7 +1,8 @@
 #!bin/bash
 sudo echo "This needs to be run as admin."
-apt-get install docker.io vsftpd -y; systemctl enable vsftpd
+apt-get install docker.io vsftpd -y; systemctl enable vsftpd docker
 cp ./vsftpd.conf /etc/vsftpd.conf; touch /etc/vsftpd.chroot_list
-chmod 444 /etc/vsftpd.chroot_list
-chmod 666 /usr/local/nwrs/scripts/var/*
+chmod 440 /etc/vsftpd.chroot_list
+chmod 660 /usr/local/nwrs/scripts/var/*
+systemctl status vsftpd docker
 
