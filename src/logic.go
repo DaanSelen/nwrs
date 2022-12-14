8766d3a1774c6cc1fb7c6077022bcb13ee7a5a1d
dselen@nerthus.nl <dselen@nerthus.nl>
2022-12-14 13:44:33 +0100

    Update 0.331

---
+++ src/logic.go
@@ -0,0 +1,131 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"os/exec"
+	"strconv"
+	"strings"
+
+	"github.com/gorilla/mux"
+)
+
+type UserRequestForm struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+type ContRequestForm struct {
+	ID       int    `json:"id"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+func main() {
+	initDB()
+	initHTTP()
+}
+
+func initHTTP() {
+	log.Println("Starting Nerthus WRS REST API-Server")
+	NWRS := mux.NewRouter().StrictSlash(true)
+
+	NWRS.HandleFunc("/", rootEndpoint).Methods("GET")
+	NWRS.HandleFunc("/nwrs/user", manipulateUser("CREATE")).Methods("POST")
+	NWRS.HandleFunc("/nwrs/user", manipulateUser("DELETE")).Methods("DELETE")
+	NWRS.HandleFunc("/nwrs/container", manipulateContainer("CREATE")).Methods("POST")
+	NWRS.HandleFunc("/nwrs/container", manipulateContainer("DELETE")).Methods("DELETE")
+	NWRS.HandleFunc("/nwrs/management/port", manipulatePort("GETPORT")).Methods("GET")
+	NWRS.HandleFunc("/nwrs/management/port", manipulatePort("RESETPORT")).Methods("PATCH")
+
+	http.ListenAndServe((":1234"), NWRS)
+}
+
+func rootEndpoint(w http.ResponseWriter, _ *http.Request) {
+	w.Header().Set("Content-Type", "text/plain")
+	json.NewEncoder(w).Encode("Root endpoint hit. Version 0.2")
+}
+
+func manipulateUser(command string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var form UserRequestForm
+		json.NewDecoder(r.Body).Decode(&form)
+		if len(form.Username) > 0 && len(form.Password) > 0 {
+			switch command {
+			case "CREATE":
+				if !checkDupl(form.Username) {
+					nextID := (getMaxID() + 1)
+					hashedPasswd := hashWithSalt(form.Password, nextID)
+					manipulateData("CREATE", strings.ToLower(form.Username), hashedPasswd)
+					executeBash("./scripts/createUser.sh -u "+strings.ToLower(form.Username)+" -p "+form.Password, true)
+					json.NewEncoder(w).Encode("CREATING USER: " + form.Username + " FINISHED")
+				} else {
+					w.WriteHeader(400)
+					json.NewEncoder(w).Encode("ERROR: Duplicate Detected (User already exists).")
+				}
+			case "DELETE":
+				if checkAuth(strings.ToLower(form.Username), form.Password) {
+					manipulateData("REMOVE", strings.ToLower(form.Username), form.Password)
+					executeBash("./scripts/removeUser.sh -u "+strings.ToLower(form.Username), true)
+					json.NewEncoder(w).Encode("REMOVING USER: " + form.Username + " FINISHED")
+				} else {
+					w.WriteHeader(401)
+					json.NewEncoder(w).Encode("ERROR: Authentication failure, or non-existing user.")
+					log.Println("Invalid or incorrect user DELETE request.")
+				}
+			}
+		} else {
+			w.WriteHeader(400)
+			json.NewEncoder(w).Encode("ERROR: Missing one (or more) required query argument(s).")
+		}
+	}
+}
+
+func manipulateContainer(command string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var form ContRequestForm
+		json.NewDecoder(r.Body).Decode(&form)
+		if len(form.Username) > 0 && len(form.Password) > 0 && form.ID > 0 {
+			if checkAuth(strings.ToLower(form.Username), form.Password) {
+				switch command {
+				case "CREATE":
+					executeBash("./scripts/createCont.sh -u "+strings.ToLower(form.Username)+" -port "+strconv.Itoa(getPort("NEXT")), true)
+					manageContainer("CREATE", form.Username)
+					json.NewEncoder(w).Encode("CREATING Container.")
+				case "DELETE":
+					executeBash("./scripts/removeCont.sh -u "+strings.ToLower(form.Username), true)
+					manageContainer("CREATE", form.Username)
+					json.NewEncoder(w).Encode("DELETE CONTAINER")
+				}
+			} else {
+				w.WriteHeader(401)
+				json.NewEncoder(w).Encode("ERROR: Authentication failure.")
+			}
+		} else {
+			w.WriteHeader(400)
+			json.NewEncoder(w).Encode("ERROR: Missing one (or more) required query argument(s).")
+		}
+	}
+}
+
+func executeBash(path string, special bool) string {
+	var out []byte
+	if special {
+		out, _ = exec.Command("/bin/bash", "-c", path).Output()
+	} else {
+		out, _ = exec.Command("/bin/bash", path).Output()
+	}
+	return (string(out))
+}
+
+func manipulatePort(command string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		switch command {
+		case "GETPORT":
+			json.NewEncoder(w).Encode(getPort("CURRENT"))
+		case "RESETPORT":
+			setPort("RESET", 0)
+			json.NewEncoder(w).Encode(getPort("CURRENT"))
+		}
+	}
+}
