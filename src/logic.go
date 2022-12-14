cdbcb7b90ae8f3a72b4cf9f98793a37fdc9d1840
dselen@nerthus.nl <dselen@nerthus.nl>
2022-12-14 13:33:16 +0100

    Update 0.33

--- src/logic.go
+++ src/logic.go
@@ -11,6 +11,16 @@ import (
 	"github.com/gorilla/mux"
 )
 
+type UserRequestForm struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+type ContRequestForm struct {
+	ID       int    `json:"id"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
 func main() {
 	initDB()
 	initHTTP()
@@ -38,26 +48,26 @@ func rootEndpoint(w http.ResponseWriter, _ *http.Request) {
 
 func manipulateUser(command string) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		uQuery, ok1 := r.URL.Query()["user"]
-		pQuery, ok2 := r.URL.Query()["pass"]
-		if ok1 || len(uQuery) > 0 && ok2 || len(pQuery) > 0 {
+		var form UserRequestForm
+		json.NewDecoder(r.Body).Decode(&form)
+		if len(form.Username) > 0 && len(form.Password) > 0 {
 			switch command {
 			case "CREATE":
-				if !checkDupl(uQuery[0]) {
+				if !checkDupl(form.Username) {
 					nextID := (getMaxID() + 1)
-					hashedPasswd := hashWithSalt(pQuery[0], nextID)
-					manipulateData("CREATE", strings.ToLower(uQuery[0]), hashedPasswd)
-					executeBash("./scripts/createUser.sh -u "+strings.ToLower(uQuery[0])+" -p "+pQuery[0], true)
-					json.NewEncoder(w).Encode("CREATING USER: " + uQuery[0] + " FINISHED")
+					hashedPasswd := hashWithSalt(form.Password, nextID)
+					manipulateData("CREATE", strings.ToLower(form.Username), hashedPasswd)
+					executeBash("./scripts/createUser.sh -u "+strings.ToLower(form.Username)+" -p "+form.Password, true)
+					json.NewEncoder(w).Encode("CREATING USER: " + form.Username + " FINISHED")
 				} else {
 					w.WriteHeader(400)
 					json.NewEncoder(w).Encode("ERROR: Duplicate Detected (User already exists).")
 				}
 			case "DELETE":
-				if checkAuth(strings.ToLower(uQuery[0]), pQuery[0]) {
-					manipulateData("REMOVE", strings.ToLower(uQuery[0]), pQuery[0])
-					executeBash("./scripts/removeUser.sh -u "+strings.ToLower(uQuery[0]), true)
-					json.NewEncoder(w).Encode("REMOVING USER: " + uQuery[0] + " FINISHED")
+				if checkAuth(strings.ToLower(form.Username), form.Password) {
+					manipulateData("REMOVE", strings.ToLower(form.Username), form.Password)
+					executeBash("./scripts/removeUser.sh -u "+strings.ToLower(form.Username), true)
+					json.NewEncoder(w).Encode("REMOVING USER: " + form.Username + " FINISHED")
 				} else {
 					w.WriteHeader(401)
 					json.NewEncoder(w).Encode("ERROR: Authentication failure, or non-existing user.")
@@ -73,19 +83,18 @@ func manipulateUser(command string) http.HandlerFunc {
 
 func manipulateContainer(command string) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		uQuery, ok1 := r.URL.Query()["user"]
-		pQuery, ok2 := r.URL.Query()["pass"]
-		iQuery, ok3 := r.URL.Query()["id"]
-		if (ok1 || len(uQuery) > 0) && (ok2 || len(pQuery) > 0) && (ok3 || len(iQuery) > 0) {
-			if checkAuth(strings.ToLower(uQuery[0]), pQuery[0]) {
+		var form ContRequestForm
+		json.NewDecoder(r.Body).Decode(&form)
+		if len(form.Username) > 0 && len(form.Password) > 0 && form.ID > 0 {
+			if checkAuth(strings.ToLower(form.Username), form.Password) {
 				switch command {
 				case "CREATE":
-					executeBash("./scripts/createCont.sh -u "+strings.ToLower(uQuery[0])+" -port "+strconv.Itoa(getPort("NEXT")), true)
-					manageContainer("CREATE", uQuery[0])
+					executeBash("./scripts/createCont.sh -u "+strings.ToLower(form.Username)+" -port "+strconv.Itoa(getPort("NEXT")), true)
+					manageContainer("CREATE", form.Username)
 					json.NewEncoder(w).Encode("CREATING Container.")
 				case "DELETE":
-					executeBash("./scripts/removeCont.sh -u "+strings.ToLower(uQuery[0]), true)
-					manageContainer("CREATE", uQuery[0])
+					executeBash("./scripts/removeCont.sh -u "+strings.ToLower(form.Username), true)
+					manageContainer("CREATE", form.Username)
 					json.NewEncoder(w).Encode("DELETE CONTAINER")
 				}
 			} else {
